d6b076d102d7087a49933e83c988c51e399a36f0
Matias Ramos <matiasramos@gmail.com>
2023-06-07 22:22:28 -0300

    envio estable

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import socketIOClient from 'socket.io-client';
+import { SERVER } from './utils/config';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [socket, setSocket] = useState(socketIOClient(SERVER));
+  const [song, setSong] = useState();
+
+  const handleChange = e => {
+    setSong(e.target.files[0]);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      const fileData = reader.result;
+      socket.emit('audio-chunk', fileData);
+    };
+    reader.readAsArrayBuffer(song);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <form onChange={handleChange} onSubmit={handleSubmit}>
+        <input type="file" name="song" />
+        <input type="submit" value="Send" />
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
